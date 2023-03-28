dab2f5b2144aea20da158986aec68e4875213551
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-28 10:23:47 +0200

    Implementation of jsonWebToken to restrict acces od data

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res) => {
     // const {name,email,password} = req.body
@@ -16,7 +16,10 @@ const register = async (req,res) => {
     // const tempUser = {name,email,password:hashedPassword}
 
     const user = await User.create({...req.body})
-    res.status(StatusCodes.CREATED).json({user})
+
+    const token =jwt.sign({userId:user._id, name:user.name}, 'jwtSecret', {expiresIn: '30d'})
+
+    res.status(StatusCodes.CREATED).json({user:{name:user.name}, token})
 }
 
 
