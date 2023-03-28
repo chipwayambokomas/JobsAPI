69d1f95d0d8eaceca7e92f8491e968247d20b9ca
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-28 08:43:18 +0200

    Routes and Controller skeletons created

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// routers
+const jobsRouter = require('./routes/jobs')
+const authRouter = require('./routes/auth')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +15,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
