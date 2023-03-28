c82f7d4edd6e634a6b5c82678c2a8437332ad584
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-28 10:05:33 +0200

    Implemented Mongoose Middleware to apply hashash process in user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,19 +3,19 @@ const {StatusCodes} = require('http-status-codes')
 const bcrypt = require('bcryptjs')
 
 const register = async (req,res) => {
-    const {name,email,password} = req.body
+    // const {name,email,password} = req.body
 
     // Create a random byte, the integer states how many random bytes which means its more secure but the larger the number the more processing power is required 
 
-    const salt = await bcrypt.genSalt(10);
+    // const salt = await bcrypt.genSalt(10);
 
     // Get what you want hashed with the salt required
-    
-    const hashedPassword = await bcrypt.hash(password, salt)
 
-    const tempUser = {name,email,password:hashedPassword}
+    // const hashedPassword = await bcrypt.hash(password, salt)
 
-    const user = await User.create({...tempUser})
+    // const tempUser = {name,email,password:hashedPassword}
+
+    const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json({user})
 }
 
