ab48a341a28ea14798ca630de500aacf177596ae
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-28 09:50:02 +0200

    Created hash pasword functionality to secure passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,21 @@
 const User = require('../models/User')
-
 const {StatusCodes} = require('http-status-codes')
+const bcrypt = require('bcryptjs')
 
 const register = async (req,res) => {
-    const user = await User.create({...req.body})
+    const {name,email,password} = req.body
+
+    // Create a random byte, the integer states how many random bytes which means its more secure but the larger the number the more processing power is required 
+
+    const salt = await bcrypt.genSalt(10);
+
+    // Get what you want hashed with the salt required
+    
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const tempUser = {name,email,password:hashedPassword}
+
+    const user = await User.create({...tempUser})
     res.status(StatusCodes.CREATED).json({user})
 }
 
--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
 
     email:{
         type: String,
-        required:[true, 'Please provide a name'],
+        required:[true, 'Please provide an email'],
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'
         ],
@@ -19,8 +19,7 @@ const UserSchema = new mongoose.Schema({
     password:{
         type: String,
         required:[true, 'Please provide a pasword'],
-        minlength: 6,
-        maxlength: 12
+        minlength: 6
     },
 })
 
