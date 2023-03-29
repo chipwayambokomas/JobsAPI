9ff312fdb9b641f648e19e15d8246621addb1e41
Saul Chipwayambokoma <chipwayambokomas@gmail.com>
2023-03-29 11:30:14 +0200

    Implementation of security packages

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
 
 // connectDB
 const connectDB = require('./db/connect')
@@ -16,7 +22,19 @@ const authRouter = require('./routes/auth')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.set('trust proxy', 1);
+app.set
+app.use(rateLimit({
+  windowMs: 15*60*1000,
+  max:100
+}))
+
 // extra packages
 
 // routes
